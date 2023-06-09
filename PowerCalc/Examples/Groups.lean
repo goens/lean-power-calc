d974939ac1b48d1cc3242e7263752475835a8001
Andres Goens <andres.goens@ed.ac.uk>
2023-06-09 13:25:59 +0100

    Add examples from group theory and rings

---
+++ PowerCalc/Examples/Groups.lean
@@ -0,0 +1,9 @@
+import Mathlib.Algebra.Group.Defs
+
+theorem inverse_involution [Group G] (g : G) : g⁻¹⁻¹ = g := by
+  calc
+    g⁻¹⁻¹ = g⁻¹⁻¹ * 1 := by rw [mul_one]
+        _ = g⁻¹⁻¹ * (g⁻¹ * g) := by rw [inv_mul_self]
+        _ = (g⁻¹⁻¹ * g⁻¹) * g := by rw [mul_assoc]
+        _ = 1 * g := by rw [inv_mul_self]
+        _ = g := by rw [one_mul]
---
+++ PowerCalc/Examples/Polynomials.lean
@@ -0,0 +1,25 @@
+import Mathlib.Algebra.Ring.Defs
+import Mathlib.Tactic.Ring
+
+theorem square_eq_two_times [CommRing R] (x : R) :
+  x^2 = x * x := by calc 
+    x^2 = Monoid.npow (Nat.succ (Nat.succ 0)) x := by simp [HPow.hPow, instHPow]
+      _ = x * (x * 1) := by rw [Monoid.npow_succ, Monoid.npow_succ, Monoid.npow_zero]
+      _ = x * x := by rw [Monoid.mul_one]
+
+theorem double_eq_two_additions [CommRing R] (x : R) :
+   2*x = x + x := by ring
+
+theorem binomial_formula [CommRing R] (x y : R) : 
+  (x + y)^2 = x^2 + 2*(x*y) + y ^2 := by
+  calc 
+    (x + y)^2 = (x + y)*(x + y) := by rw [square_eq_two_times]
+            _ = x*x + y*x + (x*y + y*y) := by rw [Distrib.left_distrib, Distrib.right_distrib, Distrib.right_distrib]
+            _ = x*x + x*y + x*y + y*y := by simp only [add_left_comm, add_assoc, mul_comm]
+            _ = x*x + (x*y + x*y) + y*y := by simp only [add_assoc]
+            _ = x*x + 2*(x*y) + y*y := by rw [← double_eq_two_additions]
+            _ = x^2 + 2*(x*y) + y^2 := by rw [square_eq_two_times, square_eq_two_times]
+
+
+theorem binomial_formula' [CommRing R] (x y : R) : 
+  (x + y)^2 = x^2 + 2*x*y + y ^2 := by ring
