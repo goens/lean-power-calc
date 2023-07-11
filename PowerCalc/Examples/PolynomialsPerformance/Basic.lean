0277511ba05ecb97e1f526fbd06338cbc1a803f9
Andres Goens <andres.goens@ed.ac.uk>
2023-07-11 22:47:34 +0100

    Update binomial

--- PowerCalc/Examples/PolynomialsPerformance/Basic.lean
+++ PowerCalc/Examples/PolynomialsPerformance/Basic.lean
@@ -1,4 +1,5 @@
 import EggTactic
+--import PowerCalc.Tactic
 -- Ring axioms
 axiom R : Type
 axiom R.add : R → R → R
@@ -28,8 +29,9 @@ notation r "!" => R.factorial r
 
 -- this should be done properly by gathering the axioms from tags and/or the environment
 set_option hygiene false in
-macro "ges" : tactic => `(tactic| egg [comm_add , comm_mul, add_assoc, mul_assoc, sub_canon, neg_add , div_canon , zero_add, zero_mul, one_mul, distribute, pow_one, pow_two, char_two] simplify (timeLimit := 3601) )
+macro "ges'" : tactic => `(tactic| egg [comm_add , comm_mul, add_assoc, mul_assoc, sub_canon, neg_add , div_canon , zero_add, zero_mul, one_mul, distribute, pow_one, pow_two, char_two] noInstantiation (timeLimit := 3601) )
 
 -- adds pow_three
 set_option hygiene false in
-macro "ges3" : tactic => `(tactic| egg [comm_add , comm_mul, add_assoc, mul_assoc, sub_canon, neg_add , div_canon , zero_add, zero_mul, one_mul, distribute, pow_one, pow_two, char_two, pow_three] simplify (timeLimit := 3601) )
+macro "ges3'" : tactic => `(tactic| egg [comm_add , comm_mul, add_assoc, mul_assoc, sub_canon, neg_add , div_canon , zero_add, zero_mul, one_mul, distribute, pow_one, pow_two, char_two, pow_three] simplify noInstantiation (timeLimit := 3601) )
+
--- PowerCalc/Examples/PolynomialsPerformance/BinomialCoeff.lean
+++ PowerCalc/Examples/PolynomialsPerformance/BinomialCoeff.lean
@@ -1,30 +1,10 @@
 import PowerCalc.Examples.PolynomialsPerformance.Basic
 
---set_option trace.EggTactic.egg true in
--- open R in
--- theorem test
---   (comm_add : forall a b : R,  a + b = b + a)
---   (comm_mul:  forall a b : R, a * b = b * a)
---   (add_assoc: forall a b c : R, a + (b + c) = (a + b) + c)
---   (mul_assoc: forall a b c : R, a * (b * c) = (a * b) * c)
---   (sub_canon: forall a b : R, (a - b)  = a + ((R.neg one) * b))
---   (neg_add : forall a : R , a + (R.neg a) = zero)
---   (div_canon : forall a b : R, (a / b) = a * (R.inv b))
---   (zero_add: forall a : R, a + zero = a )
---   (zero_mul: forall a : R, a * zero = zero)
---   (one_mul: forall a : R,  a * one = a)
---   (distribute: forall a b c : R, a * (b + c)  = (a * b) + (a * c))
---   (factorial_case : forall n : R, (n)! =  n * (n - one)!)
---   (factorial_one : forall n : R, (one)! = one )
---   (factorial_zero : forall n : R, (zero)! = one)
---   (neg_neg : forall a : R, (R.neg (R.neg a)) = a)
---   (neg_one_mul : forall a : R, (R.neg one) * a = R.neg a)
---   (binomial_helper : forall n r : R, (R.choose (n + one) r) = ((R.choose n (r - one)) + (R.choose n r)))
---   (n : R)
---   (inductive_hypothesis : forall r : R, (R.choose n r) = (n)! / ((r)! * (n - r)!)) 
---   (r : R) :
---   (n - (r - one) = (n - r + one)) := by eggcalcB -- rw [sub_canon, sub_canon, distribute (neg one), one_mul, neg_one_mul (neg one), neg_neg, neg_one_mul, ← sub_canon]
--- 
+-- adds pow_three
+set_option hygiene false in
+macro "ges'" : tactic => `(tactic| egg [comm_add , comm_mul, add_assoc, mul_assoc, sub_canon, neg_add , div_canon , zero_add, zero_mul, one_mul, distribute,  sub_cancel, mul_fractions, binomial_helper, inductive_hypothesis, factor_div, inductive_hypothesis] simplify noInstantiation (timeLimit := 1) )
+
+
 
 open R in
 theorem Rotman_1_15
@@ -33,6 +13,8 @@ theorem Rotman_1_15
   (add_assoc: forall a b c : R, a + (b + c) = (a + b) + c)
   (mul_assoc: forall a b c : R, a * (b * c) = (a * b) * c)
   (sub_canon: forall a b : R, (a - b)  = a + ((R.neg one) * b))
+  (sub_cancel : forall a : R, a - a = zero)
+  (sub_add_assoc : forall a b c : R, a + (b - c) = (a + b) - c)
   (neg_add : forall a : R , a + (R.neg a) = zero)
   (neg_neg : forall a : R, (R.neg (R.neg a)) = a)
   (neg_one_mul : forall a : R, (R.neg one) * a = R.neg a)
@@ -63,8 +45,9 @@ theorem Rotman_1_15
      calc (R.choose (n + one) r) = (R.choose n (r - one)) + (R.choose n r) := by rw [binomial_helper] 
      _ = n ! / ((r - one)! * ((n - r) + one)!) + n ! / (r ! * (n - r)!) := by rw [inductive_hypothesis (r - one), inductive_hypothesis, sub_add]
      _ = (n)! / ((r - one)! * (n - r)!) * (one / (n - r + one) + one/r) := by rw [factorial_case (n - r + one), sub_canon (n - r + one) one, ← add_assoc _ _ (neg one * one), one_mul, neg_add _, zero_add, comm_mul (n - r + one), mul_assoc, factorial_case r, comm_mul r _, ← mul_assoc _ r _, comm_mul r _, mul_assoc, ← one_mul ((n)!), ← factor_div, one_mul]
-     _ = ((n)! / ((r -one)! * (n - r)!)) * ((r + (n - r + one)) / (r * (n - r + one))) := by rw [add_fractions, comm_mul one, one_mul, one_mul, comm_mul _ r , mul_div_assoc] --by eggcalcB
-     _ = (n + one)! / r! * (n + one - r)! := by rw [mul_fractions, sub_canon, add_assoc r _ one, comm_add n _, add_assoc, neg_one_mul, neg_add, comm_add zero _, zero_add, comm_mul, factorial_case2]
+     _ = ((n)! / ((r -one)! * (n - r)!)) * ((r + (n - r + one)) / (r * (n - r + one))) := by ges' --rw [add_fractions, comm_mul one, one_mul, one_mul, comm_mul _ r , mul_div_assoc] --by gesB
+     _ = ((n)! / ((r - one)! * (n - r)!)) * ( (n + one) / (r * (n - r + one))) := by ges'
+     _ = (n + one)! / r! * (n + one - r)! := by ges'
 
 
 /-
